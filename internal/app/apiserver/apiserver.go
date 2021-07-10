06d2f89af027ca6964cb059faa53e979561abc00
vkrivoshein <vkrivoshein@mtsbank.ru>
2021-07-10 17:16:56 +0300

    add test handle and test

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,15 +1,52 @@
 package apiserver
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
+	router *gin.Engine
 }
 
 func NewAPIServer(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
+		router: gin.New(),
 	}
 }
 
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+	s.logger.Info("starting api server")
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
+}
+
+func (s *APIServer) configureRouter()  {
+	s.router.GET("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello")
+	}
 }
